854cb367c21ce34ffea4c8f41f0313d28b7cac32
Bethany <bhellerich@gmail.com>
2018-07-09 15:47:19 +0200

    commit 3

--- sketch.js
+++ sketch.js
@@ -4,8 +4,9 @@ function setup() {
 
 function draw() {
   background('purple');
+  fill("green")
   ellipse(mouseX,mouseY,100,100);
-  //triangle(30,75,58,20,86,75);
+  triangle(30,75,58,20,86,75);
   fill("orange");
   textSize(42);
   text("Hi bae! I <3 u", 100, 50);
