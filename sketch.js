4a1e4f3da3ec68b3a5262dc55898afc99120ef24
Bethany <bhellerich@gmail.com>
2018-07-09 15:30:38 +0200

    initial commit
    hey there CPH

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600,400);
+}
+
+function draw() {
+  background('purple');
+  ellipse(mouseX,mouseY,100,100);
+  //triangle(30,75,58,20,86,75);
+  fill("orange");
+  textSize(42);
+  text("hey there CPH", 100, 50);
+}
